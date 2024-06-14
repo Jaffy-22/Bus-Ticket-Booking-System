e73fcb9b1e43ed93f06a82460b9ed0bd49f57b05
Jaffy J <115729419+Jaffy-22@users.noreply.github.com>
2024-06-14 20:26:29 +0530

    Create TravelPoints.java

---
+++ TravelPoints.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ladera.travelPoints;
+
+/**
+ *
+ * @author Jaffy
+ */
+public abstract class TravelPoints {
+    public String date;
+    public String passengerName;
+    public int passengerAge;
+    public String passengerAadhar;
+    public int seatNumber;
+    public boolean[] bsrFreq;
+    public boolean[] agsFreq;
+    public boolean[] nellaiFreq;
+    public boolean[] greekFreq;
+    public abstract void sourceDestination();
+}
